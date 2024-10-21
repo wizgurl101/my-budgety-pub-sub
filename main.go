7cc2bf9bcec34fe045950a5c65cb54283a00c3fa
wizgurl101 <nb.phan@hotmail.com>
2024-10-21 14:13:37 -0600

    add more environment variables

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"my-budgety-pub-sub/utils"
+
+	"cloud.google.com/go/pubsub"
 )
 
 func main() {
 	envVariables := utils.GetEnvVariables()
-	fmt.Println("Test Env Variable:", envVariables.TestVariable)
+	fmt.Println("GCP App Credential:", envVariables.GCPAppCred)
+
+	context := context.Background()
+	client, err := pubsub.NewClient(context, envVariables.ProjectName)
+	if err != nil {
+		fmt.Println("Error creating pubsub client")
+		panic(err)
+	}
+	defer client.Close()
+
 }
