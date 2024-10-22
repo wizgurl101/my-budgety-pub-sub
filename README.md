b9b06a6eda0a375d5eb63ba1acc8b6c169290884
wizgurl101 <nb.phan@hotmail.com>
2024-10-21 18:52:10 -0600

    manage to get message from subscription

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 
 This is a Google Cloud function developed in Go lang, which purpose is to disabled billing once the budget limit been reach for My Budgety project.
 
-## Project Setup
-
 ### How to run program
 
 ```
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"my-budgety-pub-sub/utils"
+	"sync/atomic"
 
 	"cloud.google.com/go/pubsub"
 )
@@ -12,12 +13,25 @@ func main() {
 	envVariables := utils.GetEnvVariables()
 	fmt.Println("GCP App Credential:", envVariables.GCPAppCred)
 
-	context := context.Background()
-	client, err := pubsub.NewClient(context, envVariables.ProjectName)
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, envVariables.ProjectName)
 	if err != nil {
 		fmt.Println("Error creating pubsub client")
 		panic(err)
 	}
 	defer client.Close()
 
+	sub := client.Subscription(envVariables.SubscriptionId)
+
+	var received int32
+	err = sub.Receive(ctx, func(_ context.Context, msg *pubsub.Message) {
+		fmt.Printf("Got message: %q\n", string(msg.Data))
+		atomic.AddInt32(&received, 1)
+		msg.Ack()
+	})
+	if err != nil {
+		fmt.Println("Error receiving message")
+		fmt.Print(err)
+	}
+	fmt.Println("Received", received, "messages")
 }
